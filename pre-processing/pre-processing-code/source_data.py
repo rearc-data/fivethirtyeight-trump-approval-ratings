6758e0708588fcde5052e219809770336e5f28c1
Ayush <ayush.varma@rearc.io>
2020-08-25 21:04:13 -0500

    Possible fix for aws for source data

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -4,6 +4,7 @@ from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
 from multiprocessing.dummy import Pool
 from zipfile import ZipFile 
+import zipfile
 
 
 
@@ -12,7 +13,6 @@ def data_to_s3(frmt):
     # otherwise downloads and uploads to s3
 
     source_dataset_url = 'https://projects.fivethirtyeight.com/data-webpage-data/datasets/trump-approval-ratings'
-    #url_end = '?id=BUSINV'
     try:
         response = urlopen(source_dataset_url + frmt)
 
@@ -23,42 +23,42 @@ def data_to_s3(frmt):
         raise Exception('URLError: ', e.reason, frmt)
 
     else:
-      #  data_set_name = os.environ['DATA_SET_NAME']
-        data_set_name = 'AVtest'
+        data_set_name = os.environ['DATA_SET_NAME']
         filename = data_set_name + frmt
         file_location = '/tmp/' + filename
 
-        #with open(file_location, 'wb') as f:
-        #    print('hi')
-         #   f.write(response.read())
-          #  f.close()
-
-                # opening the zip file in READ mode 
+        with open(file_location, 'wb') as f:
+            f.write(response.read())
+            f.close()
+        
+        #unzips the zipped folder
         listOfFiles = []
-        with ZipFile(file_location, 'r') as zip: 
-            # printing all the contents of the zip file 
-            #zip.printdir() 
-            listOfFiles = zip.namelist()
-            # extracting all the files 
-            zip.extractall()
-            #print('Files have been extracted!') 
-        firstElem = listOfFiles[0]
-        print(firstElem)
-
+        fh = open(file_location, 'rb')
+        zipExtract = zipfile.ZipFile(fh)
+        for name in zipExtract.namelist():
+            #instead of unzipping in directory, the unzipped folder is sent to the /tmp/ folder
+            listOfFiles = zipExtract.namelist()
+            outpath = "/tmp/"
+            zipExtract.extract(name, outpath)
+        fh.close()
+        
+        #listOfFiles[0] is the name of the subfolder that holds all the files
+        folderDir = '/tmp/' + listOfFiles[0]
+    
         # variables/resources used to upload to s3
-        # s3_bucket = os.environ['S3_BUCKET']
-        # new_s3_key = data_set_name + '/dataset/'
-        # s3 = boto3.client('s3')
+        s3_bucket = os.environ['S3_BUCKET']
+        new_s3_key = data_set_name + '/dataset/'
+        s3 = boto3.client('s3')
 
-        # s3.upload_file(file_location, s3_bucket, new_s3_key + filename)
+        s3.upload_file(folderDir, s3_bucket, new_s3_key + filename)
 
-        #print('Uploaded: ' + filename)
+        print('Uploaded: ' + filename)
 
         # deletes to preserve limited space in aws lamdba
-        # os.remove(file_location)
+        os.remove(file_location)
 
         # dicts to be used to add assets to the dataset revision
-        # return {'Bucket': s3_bucket, 'Key': new_s3_key + filename}
+        return {'Bucket': s3_bucket, 'Key': new_s3_key + filename}
 
 
 def source_dataset():
@@ -75,5 +75,3 @@ def source_dataset():
     # asset_list is returned to be used in lamdba_handler function
     return asset_list
 
-
-source_dataset()
