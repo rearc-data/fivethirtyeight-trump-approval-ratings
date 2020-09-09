d7a76f611071d02ee873a7ea950c56e7226965d8
Ayush <ayush.varma@rearc.io>
2020-09-09 00:59:10 -0500

    Able to see individual files

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -5,7 +5,7 @@ from urllib.error import URLError, HTTPError
 from multiprocessing.dummy import Pool
 from zipfile import ZipFile 
 import zipfile
-
+from s3_md5_compare import md5_compare
 
 
 def data_to_s3(frmt):
@@ -23,7 +23,8 @@ def data_to_s3(frmt):
         raise Exception('URLError: ', e.reason, frmt)
 
     else:
-        data_set_name = os.environ['DATA_SET_NAME']
+        data_set_name = 'AVtest'
+        #data_set_name = os.environ['DATA_SET_NAME']
         filename = data_set_name + frmt
         file_location = '/tmp/' + filename
 
@@ -44,22 +45,45 @@ def data_to_s3(frmt):
         
         #listOfFiles[0] is the name of the subfolder that holds all the files
         folderDir = '/tmp/' + listOfFiles[0]
+
+        for filename in os.listdir('/tmp'):
+            print('name: ' + filename)
     
         # variables/resources used to upload to s3
-        s3_bucket = os.environ['S3_BUCKET']
-        new_s3_key = data_set_name + '/dataset/'
-        s3 = boto3.client('s3')
+        #s3_bucket = os.environ['S3_BUCKET']
+        #new_s3_key = data_set_name + '/dataset/'
+        #s3 = boto3.client('s3')
 
-        s3.upload_file(folderDir, s3_bucket, new_s3_key + filename)
+        s3_uploads = []
+        asset_list = []
 
-        print('Uploaded: ' + filename)
+        for r, d, f in os.walk(folderDir):
+            for filename in f:
+                
+                
+                obj_name = os.path.join(r, filename).split('/', 3).pop().replace(' ', '_').lower()
+                file_location = os.path.join(r, filename)
+                new_s3_key = data_set_name + '/dataset/' + obj_name
 
-        # deletes to preserve limited space in aws lamdba
-        os.remove(file_location)
+                has_changes = md5_compare(s3, s3_bucket, new_s3_key, file_location)
+                if has_changes:
+                    s3.upload_file(file_location, s3_bucket, new_s3_key)
+                    print('Uploaded: ' + filename)
+                else:
+                    print('No changes in: ' + filename)
 
-        # dicts to be used to add assets to the dataset revision
-        return {'Bucket': s3_bucket, 'Key': new_s3_key + filename}
+                asset_source = {'Bucket': s3_bucket, 'Key': new_s3_key}
+                s3_uploads.append({'has_changes': has_changes, 'asset_source': asset_source})
 
+        count_updated_data = sum(upload['has_changes'] == True for upload in s3_uploads)
+        if count_updated_data > 0:
+            asset_list = list(map(lambda upload: upload['asset_source'], s3_uploads))
+            if len(asset_list) == 0:
+                raise Exception('Something went wrong when uploading files to s3')
+        # asset_list is returned to be used in lamdba_handler function
+        # if it is empty, lambda_handler will not republish
+        return asset_list
+        
 
 def source_dataset():
 
@@ -75,3 +99,4 @@ def source_dataset():
     # asset_list is returned to be used in lamdba_handler function
     return asset_list
 
+source_dataset()
